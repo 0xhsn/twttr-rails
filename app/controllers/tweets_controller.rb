a8d1d156b795005fe3934cc8b6ada15901ed0375
0xhsn <mail@7asan.io>
2024-03-17 01:34:51 +0100

    first tweet: controller

---
+++ app/controllers/tweets_controller.rb
@@ -0,0 +1,15 @@
+class TweetsController < ApplicationController
+  def create
+    tweet = Tweet.new(tweet_params.merge(user: current_user))
+
+    return unless tweet.save
+
+    redirect_to root_path
+  end
+
+  private
+
+  def tweet_params
+    params.require(:tweet).permit(:body)
+  end
+end
