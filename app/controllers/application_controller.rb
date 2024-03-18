bf677d234363b8b9263ef23ca16ef9ae73ad9d76
0xhsn <mail@7asan.io>
2024-03-18 01:35:10 +0100

    add username

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,9 +2,15 @@
 
 # ApplicationController
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+  
   protected
 
   def after_sign_in_path_for(_resource)
     dashboard_path
+  end
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :email])
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,9 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable, :authentication_keys => [:username]
 
   has_many :tweets, dependent: :destroy
+
+  validates :username, uniqueness: { case_sensitive: false }
 end
