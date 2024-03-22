82622dfde9464e455423d07b90eef45db70962f4
macdoos <bigmacdoos@gmail.com>
2024-03-22 08:38:54 +0100

    refactor tweet into TweetPresenter

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -3,6 +3,8 @@
 # DashboardController
 class DashboardController < ActionController::Base
   before_action :authenticate_user!
+
   def index
+    @tweets = Tweet.order(created_at: :desc).map { |tweet| TweetPresenter.new(tweet) }
   end
 end
