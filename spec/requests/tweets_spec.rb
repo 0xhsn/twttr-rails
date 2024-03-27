788e4d6e70b596d7d7480fce13097be527a087b8
0xhsn <mail@7asan.io>
2024-03-27 07:58:54 +0100

    navigate to tweet page

--- spec/requests/tweets_spec.rb
+++ spec/requests/tweets_spec.rb
@@ -3,6 +3,15 @@
 require 'rails_helper'
 
 RSpec.describe 'Tweets', type: :request do
+  describe 'GET show' do
+    it 'succeeds' do
+      user = create(:user)
+      tweet = create(:tweet)
+      get tweet_path(tweet)
+      expect(response).to have_http_status(302) # because of turbo
+    end
+  end
+
   describe 'POST create' do
     context 'when not signed in' do
       it 'responds with redirect' do
